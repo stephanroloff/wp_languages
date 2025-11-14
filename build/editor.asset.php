5ca3fec7d87478e5865a5c09afb0f6b4acb53914
Stephan Roloff <sr@werbeagenten.de>
2025-11-14 10:32:38 +0100

    other changes

--- build/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => 'fb3cc2f30affc5636ba4');
